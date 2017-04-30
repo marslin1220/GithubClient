83333646db9afe97918905848adc24aa079ba75c
Cheng Lung Lin <marslin@gmail.com>
2017-04-30 11:20:49 +0800

    Initial Commit

---
+++ GithubClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubClient
+//
+//  Created by Lin Cheng Lung on 30/04/2017.
+//  Copyright © 2017 Lin Cheng Lung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithubClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubClient
+//
+//  Created by Lin Cheng Lung on 30/04/2017.
+//  Copyright © 2017 Lin Cheng Lung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
